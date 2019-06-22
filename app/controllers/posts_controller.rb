90ec2bf59a487ef749188b234e02bd23dd6f1981
Maya <melokitty.1003@gmail.com>
2019-06-22 22:24:39 +0900

    削除機能とバリデーション追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   # 変数は、 Rails ではビューではなく、アクションで定義することが一般的
   def index
-    @posts = Post.all
+    # descは降順という意味（新しいものから順に並び替える）
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
@@ -9,15 +10,47 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def create
     # ビューの代わりにリダイレクトさせる
     @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to("/posts/index")
+    if @post.save
+      flash[:notice] = "投稿を作成しました"
+      redirect_to("/posts/index")
+    else
+      render("posts/new")
+    end
   end
 
+  def edit
+    @post=Post.find_by(id: params[:id])
+  end
+
+  def update
+    # 最初にどのidの投稿内容を編集すれば良いか特定する
+    @post=Post.find_by(id: params[:id])
+    # 投稿内容を更新
+    @post.content = params[:content]
+    #　もし投稿がバリデーションに引っかかったら、編集ページにリダイレクトするようにする
+    # railsではフラッシュを表示するための特別な変数が用意されている。flash[:notice] = "表示したい文字列"　で書く
+    if @post.save
+      flash[:notice] = "投稿を編集しました"
+      redirect_to("/posts/index")
+    else
+      # rederメゾットを用いると、editアクションを通さず、viewを表示できる
+      # redirectさせると、editアクションを経由して、編集する前の元データの投稿を表示させてしまうため。せっかく編集した内容が水の泡になってしまう。
+      # render("フォルダー名/ファイル名")　で記述
+      render("posts/edit")
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+    flash[:notice] = "投稿を削除しました"
+    redirect_to("/posts/index")
+  end
 end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
-
+  #空の投稿を防ぐために、contentにpresence: trueを追加して、バリデーションにひっかかるようにする
+  # 140字以上の文字数は投稿できないようにしている。
+  validates :content, {presence: true, length: {maximum: 140}}
+  
 end
